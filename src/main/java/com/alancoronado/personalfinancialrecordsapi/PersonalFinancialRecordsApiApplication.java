2101101a3781b33bb67916f2362c4beae877c03d
Alan-Coronado <alancrd16@gmail.com>
2023-12-15 14:10:21 -0700

    project base structure

---
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/PersonalFinancialRecordsApiApplication.java
@@ -0,0 +1,13 @@
+package com.alancoronado.personalfinancialrecordsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancialRecordsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancialRecordsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alancoronado/personalfinancialrecordsapi/PersonalFinancialRecordsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alancoronado.personalfinancialrecordsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancialRecordsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
