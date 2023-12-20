f1d694d5ffcd3abf7e374e242589d2f17fbd102c
Alan-Coronado <alancrd16@gmail.com>
2023-12-20 14:46:00 -0700

    create record controller

---
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/controller/RecordController.java
@@ -0,0 +1,9 @@
+package com.alancoronado.personalfinancialrecordsapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("records")
+public class RecordController {
+}
