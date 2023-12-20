c1ea6a1a8b6273b7ed108754598067c7ff85f398
Alan-Coronado <alancrd16@gmail.com>
2023-12-20 15:15:38 -0700

    add delete method in record controller

--- src/main/java/com/alancoronado/personalfinancialrecordsapi/controller/RecordController.java
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/controller/RecordController.java
@@ -34,4 +34,10 @@ public class RecordController {
         return service.update(data);
     }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        service.delete(id);
+    }
+
 }
