e4e961d680dfc604c0e4e1ab2b34c14883bde1b5
Alan-Coronado <alancrd16@gmail.com>
2023-12-19 22:58:44 -0700

    add record dto

---
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/dto/RecordDTO.java
@@ -0,0 +1,11 @@
+package com.alancoronado.personalfinancialrecordsapi.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class RecordDTO {
+    private long id;
+    private BigDecimal amount;
+}
