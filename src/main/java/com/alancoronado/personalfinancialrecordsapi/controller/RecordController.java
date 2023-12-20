cca400b7ef82a1b1cd5d52004b686e87f6c51769
Alan-Coronado <alancrd16@gmail.com>
2023-12-20 14:51:34 -0700

    add save method in record controller

--- src/main/java/com/alancoronado/personalfinancialrecordsapi/controller/RecordController.java
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/controller/RecordController.java
@@ -1,9 +1,23 @@
 package com.alancoronado.personalfinancialrecordsapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alancoronado.personalfinancialrecordsapi.dto.CreateRecordDTO;
+import com.alancoronado.personalfinancialrecordsapi.dto.RecordDTO;
+import com.alancoronado.personalfinancialrecordsapi.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("records")
 public class RecordController {
+
+    @Autowired
+    RecordService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RecordDTO save(@RequestBody CreateRecordDTO data){
+        return service.save(data);
+    }
+
 }
