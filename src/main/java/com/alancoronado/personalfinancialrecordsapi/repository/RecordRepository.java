c292e14528b18d48334975a680461c86c2328264
Alan-Coronado <alancrd16@gmail.com>
2023-12-19 22:53:29 -0700

    add repository anotation

--- src/main/java/com/alancoronado/personalfinancialrecordsapi/repository/RecordRepository.java
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/repository/RecordRepository.java
@@ -2,7 +2,9 @@ package com.alancoronado.personalfinancialrecordsapi.repository;
 
 import com.alancoronado.personalfinancialrecordsapi.model.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
 }
