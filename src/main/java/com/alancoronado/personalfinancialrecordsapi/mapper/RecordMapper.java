aee94e5f6f29657f49f8e8552aa7ebd9c505a972
Alan-Coronado <alancrd16@gmail.com>
2023-12-19 23:18:58 -0700

    add method tomodel on record mapper

--- src/main/java/com/alancoronado/personalfinancialrecordsapi/mapper/RecordMapper.java
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/mapper/RecordMapper.java
@@ -1,14 +1,19 @@
 package com.alancoronado.personalfinancialrecordsapi.mapper;
 
 
+import com.alancoronado.personalfinancialrecordsapi.dto.CreateRecordDTO;
 import com.alancoronado.personalfinancialrecordsapi.dto.RecordDTO;
 import com.alancoronado.personalfinancialrecordsapi.model.Record;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface RecordMapper {
 
     RecordDTO toDTO(Record model);
+
+    @Mapping(target = "id", ignore = true)
+    Record toModel(CreateRecordDTO dto);
 
 }
