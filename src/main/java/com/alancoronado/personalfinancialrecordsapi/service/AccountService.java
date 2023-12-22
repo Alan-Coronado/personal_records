f0fcd7c6c9cff265fcf6f0d8daedb4036f8cb7f4
Alan-Coronado <alancrd16@gmail.com>
2023-12-21 20:27:32 -0700

    add method findall in account service

--- src/main/java/com/alancoronado/personalfinancialrecordsapi/service/AccountService.java
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/service/AccountService.java
@@ -8,6 +8,8 @@ import com.alancoronado.personalfinancialrecordsapi.repository.AccountRepository
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AccountService {
 
@@ -20,6 +22,10 @@ public class AccountService {
     public AccountDTO save(CreateAccountDTO data){
         Account entity = repository.save(mapper.toModel(data));
         return mapper.toDTO(entity);
+    }
+
+    public List<AccountDTO> findAll(){
+        return repository.findAll().stream().map(mapper::toDTO).toList();
     }
 
 }
