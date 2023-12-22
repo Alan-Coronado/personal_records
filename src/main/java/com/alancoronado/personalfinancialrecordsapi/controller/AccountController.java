fb10554d7d761d7605b3400fdf88bcbe993f0be7
Alan-Coronado <alancrd16@gmail.com>
2023-12-21 20:32:21 -0700

    add save method in account controller

--- src/main/java/com/alancoronado/personalfinancialrecordsapi/controller/AccountController.java
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/controller/AccountController.java
@@ -1,9 +1,22 @@
 package com.alancoronado.personalfinancialrecordsapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alancoronado.personalfinancialrecordsapi.dto.AccountDTO;
+import com.alancoronado.personalfinancialrecordsapi.dto.CreateAccountDTO;
+import com.alancoronado.personalfinancialrecordsapi.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("accounts")
 public class AccountController {
+
+    @Autowired
+    AccountService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AccountDTO save(@RequestBody CreateAccountDTO data){
+        return service.save(data);
+    }
 }
