ed1c42ab508b2122cedc2a29a1002c688d27d7c1
Alan Coronado <alancrd16@gmail.com>
2023-12-27 23:14:46 -0700

    add recorddisplaydto to display records by account

--- src/main/java/com/alancoronado/personalfinancialrecordsapi/dto/AccountDTO.java
+++ src/main/java/com/alancoronado/personalfinancialrecordsapi/dto/AccountDTO.java
@@ -2,8 +2,11 @@ package com.alancoronado.personalfinancialrecordsapi.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class AccountDTO {
     private long id;
     private String name;
+    private List<RecordDisplayDTO> records;
 }
